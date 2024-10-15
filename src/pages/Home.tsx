284ba69dc630dab414b22acbeefb42722b02ebde
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2024-10-15 18:04:45 -0400

    buggy stash

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,40 @@
 import {Link, useLocation} from 'react-router-dom';
-import { TextField } from '@mui/material';
+import {Button, TextField } from '@mui/material';
 import "../assets/css/pages/Home.css"
+import { useState } from 'react';
 
 function Home(){
 
     const isAdmin = sessionStorage.getItem("isAdmin") === "true"
+
+    const [song, setSong] = useState("")
+    const [entries, setEntries] = useState([])
 
     return(
         <div className="homePage">
 
             {isAdmin && <p>isAdmin</p>}
             <p>What are we jamming to this week?</p>
-            <div className="diaryEntiresSection">
-                <TextField id="standard-basic" label="Enter song" variant="standard" />
-                <TextField id="standard-basic" label="Enter song" variant="standard" />
-                <TextField id="standard-basic" label="Enter song" variant="standard" />
+            {entries.map((entry) => (<div>
+                <p>{entry}</p>
+            </div>))}
+            {entries.length<3 &&
+            <div className="diaryEntriesSection">
+                <TextField id="standard-basic"
+                           label="Find Song"
+                           variant="standard"
+                           value={song}
+                           onChange={(e)=>{
+                               setSong(e.target.value)
+                           }}
+                />
+                <Button onClick={()=>{
+                    console.log(song)
+                    setEntries([...entries,song])
+                }}
+                >+ Add Song</Button>
             </div>
+            }
 
         </div>
     )
